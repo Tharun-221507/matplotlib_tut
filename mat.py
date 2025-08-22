55960f2e533f978cf570eb67db1a399566717825
Tharun-221507 <tharunsaravanakumar1@gmail.com>
2025-08-22 10:40:04 +0530

    added

---
+++ mat.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3],[1,2,3], label="Linear")
+plt.plot([1,2,3],[1,4,9], label="Quadratic")
+
+plt.legend(
+    loc='upper left',
+    title="Functions",
+    fontsize=10,
+    frameon=True,
+    shadow=True,
+    ncol=1,
+    facecolor="lightgray",
+    edgecolor="blue",
+    markerscale=1.5,
+    handlelength=3,
+    handletextpad=1,
+    borderpad=1.2,
+    borderaxespad=1.5
+)
+
+plt.show()
